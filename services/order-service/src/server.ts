45d5fa0913440efcbff6d76fadbc1cf0d5ef7b9d
ItaloFL <grd788@gmail.com>
2026-04-14 12:07:55 -0500

    chore :wrench: connection and publisher event on rabbit

--- services/order-service/src/server.ts
+++ services/order-service/src/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { routes } from "./routes/routes";
 import cors from "cors";
+import { getRabbitChannel } from "./messaging/connection";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3002, () => {
-  console.log("server is running on port 3002");
-});
+async function main() {
+  await getRabbitChannel();
+  app.listen(3002, () => console.log("Order Service rodando na porta 3002"));
+}
+
+main();
