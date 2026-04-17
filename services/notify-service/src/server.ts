b293b277c2a42ae0d6273c7c9143cc07b2a77955
ItaloFL <grd788@gmail.com>
2026-04-16 20:21:36 -0500

    refactor :recycle: app error handling

--- services/notify-service/src/server.ts
+++ services/notify-service/src/server.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { startPasswordResetConsumer } from "./consumers/password-reset-consumer";
+import { AppError } from "./errors/AppError/AppError";
 
 const app = express();
 
 app.get("/health", (_, res) => res.json({ status: "ok" }));
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.code).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      message: `Server Error, ${err.message}`,
+    });
+  },
+);
 
 async function main() {
   await startPasswordResetConsumer();
